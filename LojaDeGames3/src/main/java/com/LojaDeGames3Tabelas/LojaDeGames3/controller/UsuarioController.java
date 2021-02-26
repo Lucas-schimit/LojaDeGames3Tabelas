76e71e502e718a1b77bb9ecc895780fcbb761bb4
Lucas-schimit <lucas_schimit@outlook.com>
2021-02-26 15:00:04 -0300

    feat(UsuarioController) foi criado o metodo getAll

--- LojaDeGames3/src/main/java/com/LojaDeGames3Tabelas/LojaDeGames3/controller/UsuarioController.java
+++ LojaDeGames3/src/main/java/com/LojaDeGames3Tabelas/LojaDeGames3/controller/UsuarioController.java
@@ -1,12 +1,31 @@
 package com.LojaDeGames3Tabelas.LojaDeGames3.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.LojaDeGames3Tabelas.LojaDeGames3.Model.Usuario;
+import com.LojaDeGames3Tabelas.LojaDeGames3.Repository.UsuarioRepository;
+
+
 
 @RestController
 @RequestMapping("/usuario")
 @CrossOrigin("*")
 public class UsuarioController {
+	
+	@Autowired
+	private UsuarioRepository repository;
+	
+	@GetMapping
+	public ResponseEntity<List<Usuario>> getAll(){
+		return ResponseEntity.ok(repository.findAll());
+	}
+	
 	
 }
